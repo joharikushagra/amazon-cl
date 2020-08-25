23c186e0be9be3fd4b727b81efdc5e1c42633913
Kushagra Johari <skjkushagra1309@gmail.com>
2020-08-25 15:22:20 +0530

    add products

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-import Product from '../Product/Product'
+import Product from "../Product/Product";
 // import Banner from '../images/primevideo-banner2.jpg';
 function Home() {
   return (
@@ -15,56 +15,55 @@ function Home() {
       </div>
 
       <div className="home1">
-      <div className="home-row">
-        <Product
-          id="1232141"
-          title="Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal Fabric"
-          price={11.96}
-          rating={5}
-          image="https://images-eu.ssl-images-amazon.com/images/I/61u48FEs0rL._AC_UL320_SR320,320_.jpg"
-        />
-        <Product
-          id="1232141"
-          title="Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal Fabric"
-          price={11.96}
-          rating={5}
-          image="https://images-eu.ssl-images-amazon.com/images/I/61u48FEs0rL._AC_UL320_SR320,320_.jpg"
-        />
-        <Product
-          id="1232141"
-          title="Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal Fabric"
-          price={11.96}
-          rating={5}
-          image="https://images-eu.ssl-images-amazon.com/images/I/61u48FEs0rL._AC_UL320_SR320,320_.jpg"
-        />
+        <div className="home-row">
+          <Product
+            id="1232141"
+            title="Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal Fabric"
+            price={11.96}
+            rating={5}
+            image="https://images-eu.ssl-images-amazon.com/images/I/61u48FEs0rL._AC_UL320_SR320,320_.jpg"
+          />
+          <Product
+            id="1232142"
+            title="PlayStation PS4 500GB with 3 PS Hits Game Bundle (PS4)"
+            price={254.99}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/91zXzCCzN8L._AC._SR360,460.jpg"
+          />
+          <Product
+            id="1232143"
+            title="Fitbit Versa 2 Health & Fitness Smartwatch with Voice Control, Sleep Score"
+            price={199.99}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/41Z4MrovZ1L._AC._SR360,460.jpg"
+          />
         </div>
-      <div className="home-row">
-        <Product
-          id="1232141"
-          title="Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal Fabric"
-          price={11.96}
-          rating={5}
-          image="https://images-eu.ssl-images-amazon.com/images/I/61u48FEs0rL._AC_UL320_SR320,320_.jpg"
-        />
-        <Product
-          id="1232141"
-          title="Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal Fabric"
-          price={11.96}
-          rating={5}
-          image="https://images-eu.ssl-images-amazon.com/images/I/61u48FEs0rL._AC_UL320_SR320,320_.jpg"
-        />
+        <div className="home-row">
+          <Product
+            id="1232144"
+            title="Trespass Albus Backpack, 30 Litre"
+            price={19.99}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/A16XSzp+2NL._AC._SR360,460.jpg"
+          />
+          <Product
+            id="1232145"
+            title="Van Heusen Men's Dress Shirt Regular Fit Poplin Solid"
+            price={18.00}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/61pjj7DGsTL._AC_UL320_.jpg"
+          />
         </div>
-      <div className="home-row">
-        <Product
-          id="1232141"
-          title="Echo Dot (3rd Gen) - Smart speaker with Alexa - Charcoal Fabric"
-          price={11.96}
-          rating={5}
-          image="https://images-eu.ssl-images-amazon.com/images/I/61u48FEs0rL._AC_UL320_SR320,320_.jpg"
-        />
+        <div className="home-row">
+          <Product
+            id="1232146"
+            title="Acer Aspire 5 Slim Laptop, 15.6 inches Full HD IPS Display, AMD Ryzen 3 3200U, Vega 3 Graphics, 4GB DDR4, 128GB SSD, Backlit Keyboard, Windows 10 in S Mode, A515-43-R19L,Silver"
+            price={349.99}
+            rating={4.5}
+            image="https://images-na.ssl-images-amazon.com/images/I/41vMYgD92xL._AC_US160_.jpg"
+          />
         </div>
       </div>
-      
     </>
   );
 }
