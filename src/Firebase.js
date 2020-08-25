e1356f37cb0e1ea43664c546eb238f26f8fdb685
Kushagra Johari <skjkushagra1309@gmail.com>
2020-08-25 15:40:08 +0530

    hosting

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-const firebaseApp = firebase.initializeApp({
+const firebaseApp = {
     apiKey: "AIzaSyDUFhNFgJ2m_-zx-X1g6VYprF_isc9CHuE",
     authDomain: "clone-4889a.firebaseapp.com",
     databaseURL: "https://clone-4889a.firebaseio.com",
@@ -8,7 +8,9 @@ const firebaseApp = firebase.initializeApp({
     storageBucket: "clone-4889a.appspot.com",
     messagingSenderId: "471051293738",
     appId: "1:471051293738:web:cd7f1eb4d66cc6e2f39f53"
-})
+}
+
+firebase.initializeApp(firebaseApp);
 
 // const db = firebase.firestore();
 const auth = firebase.auth();
